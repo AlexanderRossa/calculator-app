5f35946a29f8dc0d3ec8a7e61b6f4341a6a105b6
Alexander Rossa <alexanderrosssa@gmail.com>
2020-07-19 10:19:34 +0100

    Updates

---
+++ precache-manifest.3a767c5df4719a6232c525328154ec37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025d5dd777d55ca09de21cd00a98ef9b",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "9b731ee3517650898ca4",
+    "url": "/calculator-app/static/css/main.4ccbaecc.chunk.css"
+  },
+  {
+    "revision": "ace0f503e51d0c7bb99d",
+    "url": "/calculator-app/static/js/2.fd608ab8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-app/static/js/2.fd608ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b731ee3517650898ca4",
+    "url": "/calculator-app/static/js/main.d70ffd93.chunk.js"
+  },
+  {
+    "revision": "3f465b5e0c78badb6f74",
+    "url": "/calculator-app/static/js/runtime-main.d16fea3e.js"
+  }
+]);
